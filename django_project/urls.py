8e167386c55642fefcc7fbf41180a70238920006
Kylie Fannin <kf175969@umconnect.umt.edu>
2024-10-09 20:46:48 -0600

    forms, user accounts, and static files

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include #NEW
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")), #NEW
+    path("accounts/", include("accounts.urls")),
     path("", include("blog.urls")), #NEW
 ]
